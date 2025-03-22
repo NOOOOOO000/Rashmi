478faa2266d7504b6c01ad7bd154ff7c3086e8c1
NOOOOOO000 <no819154@gmail.com>
2025-03-22 22:58:31 +0530

    main.js

--- script/main.js
+++ script/main.js
@@ -125,12 +125,12 @@ const animationTimeline = () => {
         opacity: 0,
         y: -150
       },
-      "+=1.5"
+      "+=3"
     )
     .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=4")
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=7")
     .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=5")
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=7")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
@@ -138,7 +138,7 @@ const animationTimeline = () => {
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff"
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=4")
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
     .from(
